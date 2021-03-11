9c5a91f33c7b640228d7e05226f89e420d7162a6
Eli José Carrasquero <ielijose@gmail.com>
2021-03-11 08:37:25 -0300

    Fix: percentages

--- components/Table.jsx
+++ components/Table.jsx
@@ -4,24 +4,24 @@ import { formatNumber } from '../utils/numbers';
 export default function Table({ data, population }) {
   return (
     <>
-      <div className="mx-auto container px-6 py-12 w-full overflow-x-scroll xl:overflow-x-hidden">
+      <div className="container w-full px-6 py-12 mx-auto overflow-x-scroll xl:overflow-x-hidden">
         <table className="min-w-full bg-white">
           <thead>
-            <tr className="w-full h-16 bg-gray-800 py-8">
-              <th className="rounded-l-lg pl-4 md:pl-8 text-gray-300 font-normal pr-6 text-left tracking-normal leading-4">
+            <tr className="w-full h-16 py-8 bg-gray-800">
+              <th className="pl-4 pr-6 font-normal leading-4 tracking-normal text-left text-gray-300 rounded-l-lg md:pl-8">
                 Provincia
               </th>
-              <th className="text-gray-300 font-normal pr-6 text-left tracking-normal leading-4 hidden md:table-cell">
+              <th className="hidden pr-6 font-normal leading-4 tracking-normal text-left text-gray-300 md:table-cell">
                 Población
               </th>
-              <th className="text-gray-300 font-normal pr-6 text-left tracking-normal leading-4 hidden md:table-cell">
+              <th className="hidden pr-6 font-normal leading-4 tracking-normal text-left text-gray-300 md:table-cell">
                 Vacuna
               </th>
-              <th className="text-gray-300 font-normal pr-6 text-left tracking-normal leading-4">Primera Dosis</th>
-              <th className="text-gray-300 font-normal pr-6 text-left tracking-normal leading-4">% Primera Dosis</th>
-              <th className="text-gray-300 font-normal pr-6 text-left tracking-normal leading-4">Segunda Dosis</th>
+              <th className="pr-6 font-normal leading-4 tracking-normal text-left text-gray-300">Primera Dosis</th>
+              <th className="pr-6 font-normal leading-4 tracking-normal text-left text-gray-300">% Primera Dosis</th>
+              <th className="pr-6 font-normal leading-4 tracking-normal text-left text-gray-300">Segunda Dosis</th>
 
-              <th className="rounded-r-lg text-gray-300 font-normal pr-0 md:pr-8 text-left tracking-normal leading-4">
+              <th className="pr-0 font-normal leading-4 tracking-normal text-left text-gray-300 rounded-r-lg md:pr-8">
                 % Segunda Dosis
               </th>
             </tr>
@@ -31,28 +31,28 @@ export default function Table({ data, population }) {
               const provincePopulation = population[row.jurisdiccion_nombre] || 0;
 
               return (
-                <tr key={row.jurisdiccion_nombre} className="cursor-pointer h-20 border-gray-300 border-b">
-                  <td className="pl-4 md:pl-8 text-sm pr-6 whitespace-nowrap text-gray-800 tracking-normal leading-4 font-bold">
+                <tr key={row.jurisdiccion_nombre} className="h-20 border-b border-gray-300 cursor-pointer">
+                  <td className="pl-4 pr-6 text-sm font-bold leading-4 tracking-normal text-gray-800 md:pl-8 whitespace-nowrap">
                     {row.jurisdiccion_nombre}
                   </td>
 
-                  <td className="hidden md:table-cell text-sm pr-6 whitespace-nowrap text-gray-800 tracking-normal leading-4 ">
+                  <td className="hidden pr-6 text-sm leading-4 tracking-normal text-gray-800 md:table-cell whitespace-nowrap ">
                     {formatNumber(provincePopulation)}
                   </td>
-                  <td className="hidden md:table-cell text-sm pr-6 whitespace-nowrap text-gray-800 tracking-normal leading-4 ">
+                  <td className="hidden pr-6 text-sm leading-4 tracking-normal text-gray-800 md:table-cell whitespace-nowrap ">
                     {row.vacuna_nombre}
                   </td>
-                  <td className="text-sm pr-6 whitespace-nowrap text-gray-800 tracking-normal leading-4 font-semibold">
+                  <td className="pr-6 text-sm font-semibold leading-4 tracking-normal text-gray-800 whitespace-nowrap">
                     {formatNumber(row.primera_dosis_cantidad)}
                   </td>
-                  <td className="text-sm pr-6 whitespace-nowrap text-gray-800 tracking-normal leading-4 font-semibold">
-                    {formatNumber(row.primera_dosis_cantidad / provincePopulation)}%
+                  <td className="pr-6 text-sm font-semibold leading-4 tracking-normal text-gray-800 whitespace-nowrap">
+                    {formatNumber((row.primera_dosis_cantidad * 100) / provincePopulation)}%
                   </td>
-                  <td className="text-sm pr-6 whitespace-nowrap text-gray-800 tracking-normal leading-4 font-semibold">
+                  <td className="pr-6 text-sm font-semibold leading-4 tracking-normal text-gray-800 whitespace-nowrap">
                     {formatNumber(row.segunda_dosis_cantidad)}
                   </td>
-                  <td className="text-sm pr-6 whitespace-nowrap text-gray-800 tracking-normal leading-4 font-semibold">
-                    {formatNumber(row.segunda_dosis_cantidad / provincePopulation)}%
+                  <td className="pr-6 text-sm font-semibold leading-4 tracking-normal text-gray-800 whitespace-nowrap">
+                    {formatNumber((row.segunda_dosis_cantidad * 100) / provincePopulation)}%
                   </td>
                 </tr>
               );
